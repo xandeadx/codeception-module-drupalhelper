14e15b7480d94f2322ab0b15c0ece77a06ff41a4
xandeadx <xxandeadxx@gmail.com>
2020-07-16 01:34:18 +0300

    Initial commit

---
+++ src/Codeception/Module/DrupalHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Codeception\Module;
+
+use Codeception\Module as CodeceptionModule;
+
+class Db extends CodeceptionModule {
+
+  public function login($username, $password) {
+    $this->amOnPage('/user');
+    if ($this->tryToSeeElement('.user-login-form')) {
+      $this->fillField('#edit-name', $username);
+      $this->fillField('#edit-pass', $password);
+      $this->click('#edit-submit');
+    }
+  }
+
+}
