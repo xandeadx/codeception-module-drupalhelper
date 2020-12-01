1196896caab95dabda75dd583f4d9ba89290db89
xandeadx <xxandeadxx@gmail.com>
2020-12-01 08:11:31 +0300

    Refactoring

--- src/Codeception/Module/AcceptanceHelper.php
+++ src/Codeception/Module/AcceptanceHelper.php
@@ -30,21 +30,21 @@ class AcceptanceHelper extends \Codeception\Module {
   /**
    * Sql query.
    */
-  public function sqlQuery($query){
+  public function sqlQuery($query) {
     return $this->db->_getDbh()->query($query);
   }
 
   /**
    * See page title.
    */
-  public function seePageTitle($page_title) {
+  public function seePageTitle($page_title): void {
     $this->webdriver->see($page_title, ['css' => $this->config['page_title_selector']]);
   }
 
   /**
    * See list.
    */
-  public function seeList(array $items, $item_selector) {
+  public function seeList(array $items, $item_selector): void {
     foreach ($items as $key => $item) {
       $this->webdriver->see($item, ['css' => $item_selector . ':nth-child(' . ($key + 1) . ')']);
     }
@@ -53,14 +53,14 @@ class AcceptanceHelper extends \Codeception\Module {
   /**
    * See breadcrumb.
    */
-  public function seeBreadcrumb(array $items) {
+  public function seeBreadcrumb(array $items): void {
     $this->seeList($items, $this->config['breadcrumb_item_selector']);
   }
 
   /**
    * See element attribute exists or see attribute value.
    */
-  public function seeElementAttribute(string $element_selector, string $attribute_name, string $attribute_value = NULL) {
+  public function seeElementAttribute(string $element_selector, string $attribute_name, string $attribute_value = NULL): void {
     $this->webdriver->seeElementInDOM($element_selector);
     $this->webdriver->seeElementInDOM($element_selector . '[' . $attribute_name . ']');
     if ($attribute_value !== NULL) {
@@ -72,7 +72,7 @@ class AcceptanceHelper extends \Codeception\Module {
   /**
    * See element attribute not exists.
    */
-  public function dontSeeElementAttribute(string $element_selector, string $attribute_name) {
+  public function dontSeeElementAttribute(string $element_selector, string $attribute_name): void {
     $this->webdriver->seeElementInDOM($element_selector);
     $this->webdriver->dontSeeElementInDOM($element_selector . '[' . $attribute_name . ']');
   }
@@ -82,7 +82,7 @@ class AcceptanceHelper extends \Codeception\Module {
    *
    * @return string
    */
-  public function grabMaxDatabaseValue($table, $column, $where = null) {
+  public function grabMaxDatabaseValue(string $table, string $column, string $where = null): string {
     $query = "SELECT MAX($column) FROM $table";
 
     if ($where) {
@@ -95,7 +95,7 @@ class AcceptanceHelper extends \Codeception\Module {
   /**
    * Fill checkbox.
    */
-  public function fillCheckbox($checkbox, $state) {
+  public function fillCheckbox($checkbox, $state): void {
     if ($state) {
       $this->webdriver->checkOption($checkbox);
     }
@@ -107,7 +107,7 @@ class AcceptanceHelper extends \Codeception\Module {
   /**
    * Click using javascript.
    */
-  public function jsClick($selector) {
+  public function jsClick($selector): void {
     $this->webdriver->executeJS("document.querySelector('$selector').click();");
   }
 
